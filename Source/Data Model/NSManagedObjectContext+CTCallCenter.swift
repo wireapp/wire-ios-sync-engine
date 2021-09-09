01a27da6edb24764a99100425a12c9fcc0d378a8
KaterinaWire <katerina@wire.com>
2021-09-09 11:19:40 +0200

    Add internal usePackagingFeatureConfig flag

--- Source/Data Model/NSManagedObjectContext+CTCallCenter.swift
+++ Source/Data Model/NSManagedObjectContext+CTCallCenter.swift
@@ -54,4 +54,22 @@ public extension NSManagedObjectContext {
         }
         
     }
+
+    private static let PackagingFeatureConfigKey = "PackagingFeatureConfigKey"
+
+    @objc
+    var zm_usePackagingFeatureConfig : Bool {
+
+        get {
+            precondition(zm_isUserInterfaceContext, "zm_usePackagingFeatureConfig can only be accessed on the ui context")
+            return userInfo[NSManagedObjectContext.PackagingFeatureConfigKey] as? Bool ?? false
+        }
+
+        set {
+            precondition(zm_isUserInterfaceContext, "zm_usePackagingFeatureConfig can only be accessed on the ui context")
+            userInfo[NSManagedObjectContext.PackagingFeatureConfigKey] = newValue
+        }
+
+    }
+
 }
