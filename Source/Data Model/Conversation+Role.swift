b9d3c0fa296712768b519d867da9d439d17e96e7
sunbinatwire <sb88k@naver.com>
2021-11-03 09:51:59 +0100

    Add action for updating role

--- Source/Data Model/Conversation+Role.swift
+++ Source/Data Model/Conversation+Role.swift
@@ -16,67 +16,67 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-import Foundation
-
-private enum RequestError: Int, Error {
-    case unknown = 0
-}
-
-extension ZMConversation {
-
-    private typealias Factory = ConversationRoleRequestFactory
-
-    public func updateRole(of participant: UserType,
-                           to newRole: Role,
-                           session: ZMUserSession,
-                           completion: @escaping (VoidResult) -> Void) {
-
-        guard let user = participant as? ZMUser else {
-            completion(.failure(RequestError.unknown))
-            return
-        }
-        let maybeRequest = Factory.requestForUpdatingParticipantRole(newRole,
-                                                                     for: user,
-                                                                     in: self,
-                                                                     completion: completion)
-        if let request = maybeRequest {
-            session.transportSession.enqueueOneTime(request)
-        }
-    }
-}
-
-struct ConversationRoleRequestFactory {
-
-    static func requestForUpdatingParticipantRole(_ role: Role,
-                                                  for participant: ZMUser,
-                                                  in conversation: ZMConversation,
-                                                  completion: ((VoidResult) -> Void)? = nil) -> ZMTransportRequest? {
-        guard
-            let roleName = role.name,
-            let userId = participant.remoteIdentifier,
-            let conversationId = conversation.remoteIdentifier
-        else {
-            completion?(.failure(RequestError.unknown))
-            return nil
-        }
-
-        let path = "/conversations/\(conversationId.transportString())/members/\(userId.transportString())"
-        let payload = ["conversation_role": roleName]
-
-        let requestCompletionHandler = ZMCompletionHandler(on: conversation.managedObjectContext!) { response in
-            switch response.httpStatus {
-            case 200..<300:
-                conversation.addParticipantAndUpdateConversationState(user: participant, role: role)
-                conversation.managedObjectContext?.saveOrRollback()
-                completion?(.success)
-            default:
-                completion?(.failure(RequestError.unknown))
-            }
-        }
-
-        let request = ZMTransportRequest(path: path, method: .methodPUT, payload: payload as ZMTransportData)
-        request.add(requestCompletionHandler)
-
-        return request
-    }
-}
+//import Foundation
+//
+//private enum RequestError: Int, Error {
+//    case unknown = 0
+//}
+//
+//extension ZMConversation {
+//
+//    private typealias Factory = ConversationRoleRequestFactory
+//
+//    public func updateRole(of participant: UserType,
+//                           to newRole: Role,
+//                           session: ZMUserSession,
+//                           completion: @escaping (VoidResult) -> Void) {
+//
+//        guard let user = participant as? ZMUser else {
+//            completion(.failure(RequestError.unknown))
+//            return
+//        }
+//        let maybeRequest = Factory.requestForUpdatingParticipantRole(newRole,
+//                                                                     for: user,
+//                                                                     in: self,
+//                                                                     completion: completion)
+//        if let request = maybeRequest {
+//            session.transportSession.enqueueOneTime(request)
+//        }
+//    }
+//}
+//
+//struct ConversationRoleRequestFactory {
+//
+//    static func requestForUpdatingParticipantRole(_ role: Role,
+//                                                  for participant: ZMUser,
+//                                                  in conversation: ZMConversation,
+//                                                  completion: ((VoidResult) -> Void)? = nil) -> ZMTransportRequest? {
+//        guard
+//            let roleName = role.name,
+//            let userId = participant.remoteIdentifier,
+//            let conversationId = conversation.remoteIdentifier
+//        else {
+//            completion?(.failure(RequestError.unknown))
+//            return nil
+//        }
+//
+//        let path = "/conversations/\(conversationId.transportString())/members/\(userId.transportString())"
+//        let payload = ["conversation_role": roleName]
+//
+//        let requestCompletionHandler = ZMCompletionHandler(on: conversation.managedObjectContext!) { response in
+//            switch response.httpStatus {
+//            case 200..<300:
+//                conversation.addParticipantAndUpdateConversationState(user: participant, role: role)
+//                conversation.managedObjectContext?.saveOrRollback()
+//                completion?(.success)
+//            default:
+//                completion?(.failure(RequestError.unknown))
+//            }
+//        }
+//
+//        let request = ZMTransportRequest(path: path, method: .methodPUT, payload: payload as ZMTransportData)
+//        request.add(requestCompletionHandler)
+//
+//        return request
+//    }
+//}
