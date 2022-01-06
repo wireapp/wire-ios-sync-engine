b436ca1d83cfc7aa76d066333577af324f4273f3
Bill Chan <billypchan@gmail.com>
2022-01-06 17:39:45 +0100

    fix

--- Tests/Source/Notifications/PushNotifications/ZMLocalNotificationTests_Message.swift
+++ Tests/Source/Notifications/PushNotifications/ZMLocalNotificationTests_Message.swift
@@ -706,6 +706,7 @@ extension ZMLocalNotificationTests_Message {
     }
 
     func testThatItGeneratesCorrectCategoryIfEncryptionAtRestIsEnabledForTeamUser() throws {
+        // GIVEN
 #if targetEnvironment(simulator) && swift(>=5.4)
         if #available(iOS 15, *) {
             XCTExpectFailure("Expect to fail on iOS 15 simulator. ref: https://wearezeta.atlassian.net/browse/SQCORE-1188")
