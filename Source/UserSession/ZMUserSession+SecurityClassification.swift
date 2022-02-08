79a37ecb7144fe6dc6ba3dc280c851d94a1d39e8
sunbinatwire <sb88k@naver.com>
2022-02-08 18:25:11 +0100

    apply suggestions

--- Source/UserSession/ZMUserSession+SecurityClassification.swift
+++ Source/UserSession/ZMUserSession+SecurityClassification.swift
@@ -1,9 +1,19 @@
 //
-//  ZMUserSession+SecurityClassification.swift
-//  WireSyncEngine-ios
+// Wire
+// Copyright (C) 2022 Wire Swiss GmbH
 //
-//  Created by Sun Bin Kim on 02.02.22.
-//  Copyright © 2022 Zeta Project Gmbh. All rights reserved.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
 import Foundation
@@ -19,14 +29,8 @@ extension ZMUserSession {
     public func classification(with users: [UserType]) -> SecurityClassification {
         guard isSelfClassified else { return .none }
 
-        var isClassified = true
-        for user in users {
-            let classification = classification(with: user)
-
-            if classification != .classified {
-                isClassified = false
-                break
-            }
+        let isClassified = users.allSatisfy {
+            classification(with: $0) == .classified
         }
 
         return isClassified ? .classified : .notClassified
