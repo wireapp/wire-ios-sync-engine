037c2e1a6b50aa2052ed06f96914bee44873e521
sunbinatwire <sb88k@naver.com>
2022-02-10 12:09:59 +0100

    header and class name

--- Tests/Source/UserSession/ZMUserSessionTests+SecurityClassification.swift
+++ Tests/Source/UserSession/ZMUserSessionTests+SecurityClassification.swift
@@ -1,15 +1,25 @@
+////
+// Wire
+// Copyright (C) 2022 Wire Swiss GmbH
 //
-//  ZMUserSessionTests+Classification.swift
-//  UnitTests
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-//  Created by Sun Bin Kim on 10.02.22.
-//  Copyright © 2022 Zeta Project Gmbh. All rights reserved.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
 import XCTest
 @testable import WireSyncEngine
 
-final class ZMUserSessionTests_Classification: ZMUserSessionTestsBase {
+final class ZMUserSessionTests_SecurityClassification: ZMUserSessionTestsBase {
 
     func otherUser(moc: NSManagedObjectContext, domain: String?) -> ZMUser {
         let otherUser = ZMUser(context: moc)
