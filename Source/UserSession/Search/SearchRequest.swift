1ea89c2e7c588a64c73156e066ec39ff07644b8b
sunbinatwire <sb88k@naver.com>
2022-02-17 15:40:20 +0100

    adjust code

--- Source/UserSession/Search/SearchRequest.swift
+++ Source/UserSession/Search/SearchRequest.swift
@@ -78,7 +78,6 @@ public struct SearchRequest {
 
     var handleAndDomain: (String, String)? {
         let components = query.split(separator: "@")
-//            .map { String($0).trimmingCharacters(in: .whitespaces).lowercased() }
             .map { String($0).replacingOccurrences(of: " ", with: "") }
             .filter { !$0.isEmpty }
 
