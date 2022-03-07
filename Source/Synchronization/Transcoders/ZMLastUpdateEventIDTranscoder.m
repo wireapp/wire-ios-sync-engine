11bbf8e90312095702b2814b3dbc8cdc2912839f
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2022-03-07 22:37:07 +0100

    fix tests

--- Source/Synchronization/Transcoders/ZMLastUpdateEventIDTranscoder.m
+++ Source/Synchronization/Transcoders/ZMLastUpdateEventIDTranscoder.m
@@ -107,7 +107,7 @@
     // no op
 }
 
-- (ZMTransportRequest *)requestForSingleRequestSync:(ZMSingleRequestSync *)sync
+- (ZMTransportRequest *)requestForSingleRequestSync:(ZMSingleRequestSync *)sync apiVersion:(APIVersion)apiVersion
 {
     NOT_USED(sync);
     NSURLComponents *components = [NSURLComponents componentsWithString:@"/notifications/last"];
@@ -117,7 +117,7 @@
         components.queryItems = @[[NSURLQueryItem queryItemWithName:@"client" value:selfClient.remoteIdentifier]];
     }
     
-    return [ZMTransportRequest requestGetFromPath:components.string apiVersion:0];
+    return [ZMTransportRequest requestGetFromPath:components.string apiVersion:apiVersion];
 }
 
 - (void)didReceiveResponse:(ZMTransportResponse *)response forSingleRequest:(ZMSingleRequestSync *)sync
