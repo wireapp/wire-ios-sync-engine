9d9f7733deadcad807fb78a25d212824f62c1e47
David Henner <david.henner@wire.com>
2022-05-06 12:09:26 +0200

    WIP

--- Source/SessionManager/SessionManager+APIVersionResolver.swift
+++ Source/SessionManager/SessionManager+APIVersionResolver.swift
@@ -18,6 +18,9 @@
 
 import Foundation
 
+// TODO: David - move to Logging extension in RS
+private let log = ZMSLog(tag: "APIVersion")
+
 extension SessionManager: APIVersionResolverDelegate {
 
     public func resolveAPIVersion() {
@@ -42,6 +45,52 @@ extension SessionManager: APIVersionResolverDelegate {
 
     func apiVersionResolverFailedToResolveVersion(reason: BlacklistReason) {
         delegate?.sessionManagerDidBlacklistCurrentVersion(reason: reason)
+    }
+
+    func apiVersionResolverDetectedFederationHasBeenEnabled() {
+
+        delegate?.sessionManagerWillMigrateAccount { [weak self] in
+            guard let `self` = self else { return }
+
+            let accountMigrationGroup = DispatchGroup()
+
+            self.activeUserSession = nil
+            self.accountManager.accounts.forEach { account in
+                accountMigrationGroup.enter()
+                // 1. Tear down the user sessions
+                self.tearDownBackgroundSession(for: account.userIdentifier)
+
+                // 2. do the migration
+                CoreDataStack.migrateLocalStorage(
+                    accountIdentifier: account.userIdentifier,
+                    applicationContainer: self.sharedContainerURL,
+                    dispatchGroup: self.dispatchGroup,
+                    migration: { context in
+                        // extend the context to perform users and conversations migration
+                    },
+                    completion: { result in
+                        if case let .failure(error) = result {
+                            log.error("Failed to migrate account: \(error)")
+                        }
+
+                        accountMigrationGroup.leave()
+                    }
+                )
+            }
+
+            accountMigrationGroup.wait()
+
+            // 3. Activate the selected account session
+            guard let selectedAccount = self.accountManager.selectedAccount else {
+                // If no selected account, we may want to load an unauthenticated session
+                // and report to the UI so the app can get out of the migration state
+                return
+            }
+            // When completed, this should trigger an AppState change through the SessionManagerDelegate
+            self.loadSession(for: selectedAccount, completion: { _ in })
+
+            // 4. Load the other accounts background sessions?
+        }
     }
 
 }
