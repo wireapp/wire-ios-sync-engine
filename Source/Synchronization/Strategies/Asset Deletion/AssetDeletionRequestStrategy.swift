dce31ec8fbaf7731ca2a82f058c2968fd7069008
Ghulam Mustafa <ghulam.mustafa@wire.com>
2022-06-30 11:19:28 +0200

    added case for version support v2

--- Source/Synchronization/Strategies/Asset Deletion/AssetDeletionRequestStrategy.swift
+++ Source/Synchronization/Strategies/Asset Deletion/AssetDeletionRequestStrategy.swift
@@ -20,7 +20,18 @@ import Foundation
 
 fileprivate extension AssetRequestFactory {
     static func request(for identifier: String, on queue: ZMSGroupQueue, apiVersion: APIVersion, block: @escaping ZMCompletionHandlerBlock) -> ZMTransportRequest {
-        let request = ZMTransportRequest(path: "/assets/v3/\(identifier)", method: .methodDELETE, payload: nil, apiVersion: apiVersion.rawValue)
+
+        let path: String
+
+        switch apiVersion {
+        case .v0, .v1:
+            path = "/assets/v3/\(identifier)"
+        case .v2:
+            let domain = APIVersion.domain ?? ""
+            path = "/assets/\(domain)/\(identifier)"
+        }
+
+        let request = ZMTransportRequest(path: path, method: .methodDELETE, payload: nil, apiVersion: apiVersion.rawValue)
         request.add(ZMCompletionHandler(on: queue, block: block))
         return request
     }
