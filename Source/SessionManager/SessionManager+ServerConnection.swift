4b37517643b3a8c717a847a3dfee75f758312b99
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2022-07-05 23:58:34 +0200

    delete dead code

--- Source/SessionManager/SessionManager+ServerConnection.swift
+++ Source/SessionManager/SessionManager+ServerConnection.swift
@@ -19,20 +19,11 @@
 import Foundation
 
 @objc
-public protocol ServerConnectionObserver {
-
-    @objc(serverConnectionDidChange:)
-    func serverConnection(didChange serverConnection: ServerConnection)
-
-}
-
-@objc
 public protocol ServerConnection {
 
     var isMobileConnection: Bool { get }
     var isOffline: Bool { get }
 
-    func addServerConnectionObserver(_ observer: ServerConnectionObserver) -> Any
 }
 
 extension SessionManager {
@@ -53,13 +44,5 @@ extension SessionManager: ServerConnection {
         return reachability.isMobileConnection
     }
 
-    /// Add observer of server connection. Returns a token for de-registering the observer.
-    public func addServerConnectionObserver(_ observer: ServerConnectionObserver) -> Any {
-
-        return reachability.addReachabilityObserver(on: .main) { [weak self, weak observer] _ in
-            guard let `self` = self else { return }
-            observer?.serverConnection(didChange: self)
-        }
-    }
 
 }
