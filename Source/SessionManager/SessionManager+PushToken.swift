1f430cbb16fe8e18df768263af4ecce53ecdbb0c
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2022-12-09 16:58:53 +0100

    Update Source/SessionManager/SessionManager+PushToken.swift
    Co-authored-by: Tommaso Piazza <196761+tmspzz@users.noreply.github.com>

--- Source/SessionManager/SessionManager+PushToken.swift
+++ Source/SessionManager/SessionManager+PushToken.swift
@@ -27,7 +27,7 @@ extension SessionManager {
      *    - Standard APNS pushes -> delivered to Notification Service Extension (NSE).
      *    - Still register for voIP pushes (BUT DON'T REGISTER WITH BACKEND), so that
      *      the NSE can wake up the main app to notify calls to CallKit (via PushKit).
-     *    - Why? VoIP pushes are restricted for calling only since iOS 13, out exemption
+     *    - Why? VoIP pushes are restricted for calling only since iOS 13, our exemption
      *      expires in iOS 15.
      *
      * iOS 14 and earlier:
